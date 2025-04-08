367e372116d7217d1c968819aec2e69d3908d5e4
Shiran Indika Karunasena <shiranindika90@gmail.com>
2025-04-08 21:28:57 +0530

    Adding Main files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Initialize AOS (Animate on Scroll)
+AOS.init();
+
+// Toggle mobile nav menu
+function toggleMenu() {
+  const nav = document.getElementById("mobileNav");
+  nav.classList.toggle("hidden");
+}
